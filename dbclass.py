af6f1cb1f6a9f4c35b5027f8e47911bda6eebcdc
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-10-13 19:30:42 -0700

    updated vercel kv prefixes

--- dbclass.py
+++ dbclass.py
@@ -3,7 +3,7 @@ class db_:
   def __init__(self):
     return
   def __getitem__(self, key):
-    return json.loads(requests.get(os.environ['KV_REST_API_URL'] + '/get/' + str(key), headers={'Authorization':os.environ['KV_REST_API_TOKEN']}).json()['result'])
+    return json.loads(requests.get(os.environ['KV2NEW_REST_API_URL'] + '/get/' + str(key), headers={'Authorization':os.environ['KV2NEW_REST_API_TOKEN']}).json()['result'])
   def __setitem__(self, key, value):
-    print(requests.post(os.environ['KV_REST_API_URL'] + '/set/' + key, data=json.dumps(value), headers={'Authorization':os.environ['KV_REST_API_TOKEN']}).json())
+    print(requests.post(os.environ['KV2NEW_REST_API_URL'] + '/set/' + key, data=json.dumps(value), headers={'Authorization':os.environ['KV2NEW_REST_API_TOKEN']}).json())
 db = db_()
